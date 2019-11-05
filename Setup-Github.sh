04710fa6fad27a371e73518bd6e6dddc4928eb09
Tyrone Russ <tyroneruss.aws#gmail.com>
2019-11-04 18:08:57 -0600

    Remove .dea folder

--- Setup-Github.sh
+++
@@ -1,9 +0,0 @@
-# Configuring Github
-Step1: create personal token -- so you don't have to type is username/password       Right upper corner -> settings -> Developers settings -> Personal token       -> Generate new token ... 
->git clone https://github.com/btctruss/fclistingservice.git
-
-
-Using git bash> ssh-keygen -t rsa -b 4096 -C "lenorsem@bigtechconsultants.net"
-
-
-Create a branch: TreDev (what ever name)
